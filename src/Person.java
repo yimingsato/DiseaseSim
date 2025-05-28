d7e85a252ad36e382313e9e18e134e6fd794145a
Adam ZHANG <AdamZhang@YL-MacBook-Air.local>
2025-05-28 14:04:58 -0400

    Person class added, test

---
+++ src/Person.java
@@ -0,0 +1,18 @@
+public class Person {
+    String name;
+    int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + '}';
+    }             
+}
