687e65c64694f4be9003e091247b79bcb5c2c86e
yimingsato <yimingsato@gmail.com>
2025-06-02 13:30:22 -0400

    test

--- src/Bacteria.java
+++ src/Bacteria.java
@@ -1,7 +1,7 @@
 public class Bacteria extends Disease{
     public Bacteria(String name, int diseaseID, int transmissionRate, int mortalityRate) {
         super(name, diseaseID, transmissionRate, mortalityRate);
-        String test;
-        String testing;
+        System.out.println("testing");
     }
+    
 }
