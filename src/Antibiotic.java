0054b7955eb7c1cfcdd63c257a0fb4ea5ccf3bd9
amayanak <amaya.nakashima@student.tdsb.on.ca>
2025-06-02 13:29:02 -0400

    change

--- src/Antibiotic.java
+++ src/Antibiotic.java
@@ -1,4 +1,3 @@
 public class Antibiotic extends Cure {
-    String test;
-    String testing;
+
 }
--- src/RunSim.java
+++ src/RunSim.java
@@ -1,5 +1,7 @@
 public class RunSim {
     public static void main(String[] args) {
         System.out.print("hi");
+
+        System.out.println();
     }
 }
