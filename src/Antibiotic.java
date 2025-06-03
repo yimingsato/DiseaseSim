ca6f9c9995fcf73b5e9501c81f19af3034de514f
amayanak <amaya.nakashima@student.tdsb.on.ca>
2025-06-03 14:08:53 -0400

    vaccine stuff

--- src/Antibiotic.java
+++ src/Antibiotic.java
@@ -1,2 +1,29 @@
 public class Antibiotic extends Cure {
+    
+    //Accessor mutator
+    public String getName() {
+        return name;
+    }
+    public int getEfficacyRate() {
+        return efficacyRate;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEfficacyRate(int efficacyRate) {
+        this.efficacyRate = efficacyRate;
+    }
+    //Constructor
+    public Antibiotic(String name, int efficacyRate) {
+        super(name, efficacyRate);
+    }
+
+public boolean applyTo(Person person) {
+    if (person.isInfected()) {
+        person.setHealthStatus("Recovered");
+        return true;
+    }
+    return false;
+}
+
 }
--- src/Vaccine.java
+++ src/Vaccine.java
@@ -1,3 +1,36 @@
 public class Vaccine extends Cure{
-    
+
+    // Accessor mutator
+    public String getName() {
+        return name;
+    }
+    public int getEfficacyRate() {
+        return efficacyRate;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEfficacyRate(int efficacyRate) {
+        this.efficacyRate = efficacyRate;
+    }
+
+    // Constructor
+    public Vaccine(String name, int efficacyRate) {
+        super(name, efficacyRate);
+    }
+
+    public boolean applyTo(Person person) {
+        if (!person.isInfected()) {
+            person.setHealthStatus(true);
+            return true; // Vaccine applied successfully
+        }
+        return false; // Person is already vaccinated
+    }
+
+    public String toString() {
+        return "Vaccine{" +
+                "name='" + name + '\'' +
+                ", efficacyRate=" + efficacyRate +
+                '}';
+    }
 }
