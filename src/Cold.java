d81c6f3ccbb27d30ad07bcd7609de790294b4ae0
amayanak <amaya.nakashima@student.tdsb.on.ca>
2025-06-04 12:29:20 -0400

    cold and hot regions

--- src/Cold.java
+++ src/Cold.java
@@ -1,4 +1,10 @@
 public class Cold extends Region{
+    private static boolean snowCoverage;
+    private static boolean crowdedIndoors;
     
-    
+    //Accessor/Mutator
+//Cold(String, double, int, double, int, int)
+//calcInfectionRateInfluence(int baseInfectionRate): double
+//toString(): String
+
 }
--- src/Hot.java
+++ src/Hot.java
@@ -1,3 +1,9 @@
 public class Hot extends Region{
-    
+    private static boolean healthySunExposure;
+    private static boolean dryClimate;
+
+    //Accessor/Mutator
+//Hot(String, double, int, double, int, int)
+//calcInfectionRateInfluence(int baseInfectionRate ): double
+
 }
