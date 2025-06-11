bd8aacd04b2d867f0fe58cc49e19544108bfe386
amayanak <amaya.nakashima@student.tdsb.on.ca>
2025-06-11 14:26:32 -0400

    runsim

--- src/RunSim.java
+++ src/RunSim.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class RunSim {
     
     public static void main(String[] args) {
@@ -7,10 +9,22 @@ public class RunSim {
         SimulationMap simulationMap = new SimulationMap(0,0);
 
         System.out.println("Welcome to the Disease Simulation!");
-        System.out.println("Enter file for");
+        System.out.println("Enter file for Disease data:");
         String filename = sc.nextLine();
+        diseaseManager.loadDisease(filename);
+        System.out.println("Enter file for Cure data:");
+        filename = sc.nextLine();
+        cureManager.loadCures(filename);
+        System.out.println("Enter file for Simulation Map data:");
+        filename = sc.nextLine();
+        simulationMap.loadMap(filename);
+        System.out.println("Loaded successfully!");
 
-        //diseaseManager.loadDisease(filename);
+        System.out.println("Enter the number of days to run the simulation:");
+        int days = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+        
+        
 
     }
 }
