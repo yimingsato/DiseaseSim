a5ae175703e676c2ba568c81b295548ebbdcfb8b
amayanak <amaya.nakashima@student.tdsb.on.ca>
2025-06-11 14:14:10 -0400

    main

--- src/RunSim.java
+++ src/RunSim.java
@@ -1,6 +1,16 @@
 public class RunSim {
     
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        DiseaseManager diseaseManager = new DiseaseManager();
+        CureManager cureManager = new CureManager();
+        SimulationMap simulationMap = new SimulationMap(0,0);
+
+        System.out.println("Welcome to the Disease Simulation!");
+        System.out.println("Enter file for");
+        String filename = sc.nextLine();
+
+        //diseaseManager.loadDisease(filename);
+
     }
 }
